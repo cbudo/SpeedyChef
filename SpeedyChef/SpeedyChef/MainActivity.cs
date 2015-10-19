fba09890d624ed23266f72edc6cfcacbd037087b
Stephen Trotta <trottasn@rose-hulman.edu>
2015-10-18 22:22:46 -0400

    Initial App Commit

---
+++ SpeedyChef/SpeedyChef/MainActivity.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace SpeedyChef
+{
+	[Activity (Label = "SpeedyChef", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button menu_button = FindViewById<Button> (Resource.Id.menu_button);
+			
+			menu_button.Click += (s, arg) => {
+				menu_button.SetBackgroundResource(Resource.Drawable.pressed_lines);
+				PopupMenu menu = new PopupMenu (this, menu_button);
+				menu.Inflate (Resource.Menu.Main_Menu);
+				menu.MenuItemClick += (s1, arg1) => {
+					Console.WriteLine ("{0} selected", arg1.Item.TitleFormatted);
+				};
+
+				menu.DismissEvent += (s2, arg2) => {
+					menu_button.SetBackgroundResource(Resource.Drawable.menu_lines);
+					Console.WriteLine ("menu dismissed"); 
+				};
+
+				menu.Show ();
+			};
+		}
+	}
+}
+
+
