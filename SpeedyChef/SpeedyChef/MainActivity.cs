12761c70dfaaeac178093aad0fb787411fc5df42
Stephen Trotta <trottasn@rose-hulman.edu>
2015-10-19 22:13:15 -0400

    Exciting and Amazing RecyclerView implementation. So proud...

--- SpeedyChef/SpeedyChef/MainActivity.cs
+++ SpeedyChef/SpeedyChef/MainActivity.cs
@@ -6,20 +6,34 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using v7Widget = Android.Support.V7.Widget;
+using System.Collections.Generic;
 
 namespace SpeedyChef
 {
 	[Activity (Theme="@style/MyTheme", Label = "SpeedyChef", MainLauncher = true, Icon = "@drawable/icon")]
 	public class MainActivity : Activity
 	{
-		//int count = 1;
-
+		v7Widget.RecyclerView mRecyclerView;
+		v7Widget.RecyclerView.LayoutManager mLayoutManager;
+		TestAdapter mAdapter;
+		TestObject mObject;
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
 
+			mObject = new TestObject ();
+
+			mAdapter = new TestAdapter (mObject);
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
+
+			mRecyclerView = FindViewById<v7Widget.RecyclerView> (Resource.Id.recyclerView);
+
+			mRecyclerView.SetAdapter (mAdapter);
+
+			mLayoutManager = new v7Widget.LinearLayoutManager (this);
+			mRecyclerView.SetLayoutManager (mLayoutManager);
 
 			Button menu_button = FindViewById<Button> (Resource.Id.menu_button);
 
@@ -39,6 +53,75 @@ namespace SpeedyChef
 
 				menu.Show ();
 			};
+		}
+	}
+
+	public class TestViewHolder : v7Widget.RecyclerView.ViewHolder
+	{
+		public TextView Caption { get; private set; }
+		// Locate and cache view references
+		public TestViewHolder (View itemView) : base (itemView)
+		{
+			Caption = itemView.FindViewById<TextView> (Resource.Id.textView);
+		}
+	}
+
+	public class TestAdapter : v7Widget.RecyclerView.Adapter
+	{
+		public TestObject mTestObject;
+
+		public TestAdapter (TestObject inTestObject)
+		{
+			mTestObject = inTestObject;
+		}
+
+		public override v7Widget.RecyclerView.ViewHolder
+		OnCreateViewHolder (ViewGroup parent, int viewType)
+		{
+			View itemView = LayoutInflater.From (parent.Context).
+				Inflate (Resource.Layout.TestCardView, parent, false);
+			TestViewHolder vh = new TestViewHolder (itemView);
+			return vh;
+		}
+
+		public override void
+		OnBindViewHolder (v7Widget.RecyclerView.ViewHolder holder, int position)
+		{
+			TestViewHolder vh = holder as TestViewHolder;
+			vh.Caption.Text = mTestObject.getObjectInPosition(position);
+		}
+
+		public override int ItemCount
+		{
+			get { return mTestObject.NumElements; }
+		}
+	}
+
+	public class TestObject
+	{
+		public int NumElements;
+		public string[] CaptionArray;
+
+		public TestObject ()
+		{
+			CaptionArray = new string[11];
+			CaptionArray [0] = "hello";
+			CaptionArray [1] = "this";
+			CaptionArray [2] = "is";
+			CaptionArray [3] = "a";
+			CaptionArray [4] = "test";
+			CaptionArray [5] = "six";
+			CaptionArray [6] = "seven";
+			CaptionArray [7] = "eight";
+			CaptionArray [8] = "nine";
+			CaptionArray [9] = "ten";
+			CaptionArray [10] = "eleven";
+			NumElements = CaptionArray.Length;
+		}
+
+		public string getObjectInPosition(int position)
+		{
+			return this.CaptionArray [position];
 		}
 	}
 }
