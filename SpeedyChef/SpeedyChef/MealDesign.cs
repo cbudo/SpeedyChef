8d91615529f983eec6d86b84fb8760b9eb9c1f1b
Larry Gates <gateslm@rose-hulman.edu>
2015-10-22 01:22:51 -0400

    Made progress
    Got button handling working.

---
+++ SpeedyChef/SpeedyChef/MealDesign.cs
@@ -0,0 +1,59 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace SpeedyChef
+{
+	[Activity (Label = "MealDesign")]			
+	public class MealDesign : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+			SetContentView (Resource.Layout.MealDesign);
+			// Create your application here
+			long binaryDate = Intent.GetLongExtra("Date", 0);
+			DateTime date = DateTime.FromBinary (binaryDate);
+			TextView text = FindViewById<TextView> (Resource.Id.debug);
+			text.Text = date.ToString ();
+
+			//MENU VIEW
+			Button menu_button = FindViewById<Button> (Resource.Id.menu_button);
+			menu_button.Click += (s, arg) => {
+				menu_button.SetBackgroundResource(Resource.Drawable.pressed_lines);
+				PopupMenu menu = new PopupMenu (this, menu_button);
+				menu.Inflate (Resource.Menu.Main_Menu);
+				menu.MenuItemClick += (s1, arg1) => {
+					// Console.WriteLine ("{0} selected", arg1.Item.TitleFormatted);
+					if (arg1.Item.TitleFormatted.ToString() == "Browse") {
+						var intent = new Intent(this, typeof(BrowseNationalitiesActivity));
+						StartActivity(intent);
+					}
+					else if (arg1.Item.TitleFormatted.ToString() == "Plan") {
+						var intent = new Intent(this, typeof(MealPlannerCalendar));
+						StartActivity(intent);
+					}
+					else if (arg1.Item.TitleFormatted.ToString() == "Walkthrough"){
+						var intent = new Intent(this, typeof(StepsActivity));
+						StartActivity(intent);
+					}
+				};
+				menu.DismissEvent += (s2, arg2) => {
+					menu_button.SetBackgroundResource(Resource.Drawable.menu_lines);
+					Console.WriteLine ("menu dismissed");
+				};
+				menu.Show ();
+			};
+		}
+	}
+}
+
